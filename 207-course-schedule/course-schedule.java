ef9c8338f0e78e7bb2dfc4e2f4ac54bbbd443fee
SaiRuchithaHT <137959842+SaiRuchithaHT@users.noreply.github.com>
2025-07-14 18:42:43 -0400

    Time: 3 ms (99.84%) | Memory: 44.5 MB (99.19%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,28 +1,25 @@
 class Solution {
-    public boolean dfs(int[] state, int course, List<Integer>[] adj){
+    public boolean dfs(int course, List<Integer>[] list, int[] state){
         state[course] = 1;
-        for(int neighbour : adj[course]){
-            if(state[neighbour]==1)
+        for(Integer neigh : list[course]){
+            if(state[neigh]==1)
                 return false;
-            else if(state[neighbour]==0){
-                if(!dfs(state, neighbour, adj))
-                    return false;
-            }
+            if(state[neigh]==0 && !dfs(neigh, list, state))
+                return false;
         }
         state[course] = 2;
         return true;
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] state = new int[numCourses];
-        List<Integer>[] adj = new ArrayList[numCourses];
-        for(int i=0; i<numCourses; i++){
-            adj[i] = new ArrayList<>();
+        List<Integer>[] list = new ArrayList[numCourses];
+        for(int i=0; i<numCourses; i++)
+            list[i] = new ArrayList<>();
+        for(int[] req : prerequisites){
+            list[req[0]].add(req[1]);
         }
-        for(int[] i : prerequisites){
-            adj[i[0]].add(i[1]);
-        }
         for(int i=0; i<numCourses; i++){
-            if(state[i]==0 && !dfs(state, i, adj))
+            if(state[i]==0 && !dfs(i, list, state))
                 return false;
         }
         return true;
