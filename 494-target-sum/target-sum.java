b261cb85f0cde43f2e383e34757fb932f0336ccf
SaiRuchithaHT <137959842+SaiRuchithaHT@users.noreply.github.com>
2025-08-07 22:57:35 -0400

    Time: 665 ms (6.29%) | Memory: 41.6 MB (69.05%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,21 +1,17 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        counter.put(0, 1);
-
-        for (int n : nums) {
-            Map<Integer, Integer> temp = new HashMap<>();
-
-            for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-                int total = entry.getKey();
-                int count = entry.getValue();
-
-                temp.put(total + n, temp.getOrDefault(total + n, 0) + count);
-                temp.put(total - n, temp.getOrDefault(total - n, 0) + count);
-            }
-            counter = temp;
+    int count = 0;
+    public void recursive(int index, int n, int[] nums, int target){
+        if(target==0 && index==n){
+            count++;
+            return;
         }
-
-        return counter.getOrDefault(target, 0);        
+        if(index==n)
+            return;
+        recursive(index+1, nums.length, nums, target+nums[index]);
+        recursive(index+1, nums.length, nums, target-nums[index]);
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        recursive(0, nums.length, nums, target);
+        return count;
     }
 }
