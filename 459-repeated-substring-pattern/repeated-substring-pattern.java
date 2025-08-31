93e370182ca5c0b53411ccd273b1e42e44f0fc17
SaiRuchithaHT <137959842+SaiRuchithaHT@users.noreply.github.com>
2025-08-31 12:26:57 -0400

    Time: 75 ms (46.27%) | Memory: 45.2 MB (74.47%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,15 +1,7 @@
-class Solution {
+public class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        int n = s.length();
-        for(int i=1; i<=n/2; i++){
-            String sub = s.substring(0,i);
-            int j = i;
-            while(s.indexOf(sub, j)==j){
-                j = j+sub.length();
-                if(j==s.length())
-                    return true;
-            }
-        }
-        return false;
+        String doubled = s + s;
+        String sub = doubled.substring(1, doubled.length() - 1);
+        return sub.contains(s);
     }
 }
